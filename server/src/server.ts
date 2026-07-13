d7647cc48a34ed9189017bcba5923db74ae26c2d
ganeshmakade-59 <makadeganesh59@gmail.com>
2026-07-14 00:24:40 +0530

    fix: bypass network DNS restriction for MongoDB connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,6 @@
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 import app from "./app.ts";
 import connectDB from "./config/database.config.ts";
 import { initializeKnowledgeBase } from "./services/rag.service.ts";
